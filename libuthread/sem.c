8e6d5c768f98714e84a01a1ab7d4c27f5477e808
ejayp <elprotacio@ucdavis.edu>
2019-05-06 15:52:24 -0700

    Added most of the functionality but stalls

--- libuthread/sem.c
+++ libuthread/sem.c
@@ -5,32 +5,79 @@
 #include "sem.h"
 #include "thread.h"
 
+
 struct semaphore {
-	/* TODO Phase 1 */
+	int count;
+	queue_t blocked_queue;
 };
 
 sem_t sem_create(size_t count)
 {
-	/* TODO Phase 1 */
+	sem_t sem;
+	sem = (sem_t) malloc(sizeof(sem_t));
+
+	if(sem){
+		sem->count = count;
+		return sem;
+	}
+
+	sem->count = count;
+	sem->blocked_queue = queue_create();
+	return NULL;
 }
 
 int sem_destroy(sem_t sem)
 {
-	/* TODO Phase 1 */
+	if(sem == NULL || queue_length(sem->blocked_queue) > 0)
+		return -1;
+
+	free(sem);
+	return 0;
 }
 
 int sem_down(sem_t sem)
 {
-	/* TODO Phase 1 */
+	int tid = pthread_self();
+
+	if(!sem)
+		return -1;
+
+	//Blocks current thread if no semaphore
+	if(sem->count == 0){
+		queue_enqueue(sem->blocked_queue, &tid); 
+		thread_block();
+	}
+
+	sem->count--;
+	return 0;	
 }
 
 int sem_up(sem_t sem)
 {
-	/* TODO Phase 1 */
+	int tid;
+
+	if(!sem)
+		return -1;
+
+	//Unblocks oldest thread if semaphore
+	if(queue_length(sem->blocked_queue) > 0){
+		queue_dequeue(sem->blocked_queue, (void**) &tid);
+		thread_unblock(tid);
+	}
+
+	sem->count++;
+	return 0;	
 }
 
 int sem_getvalue(sem_t sem, int *sval)
 {
-	/* TODO Phase 1 */
-}
+	if(!sem || !sval)
+		return -1;
 
+	if(sem->count > 0)
+		*sval = sem->count;
+	
+	else if(sem->count == 0)
+		*sval = queue_length(sem->blocked_queue) * -1;
+	return 0;
+}
